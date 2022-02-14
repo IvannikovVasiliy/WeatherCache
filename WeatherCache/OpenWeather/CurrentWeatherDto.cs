9f3ada359d6328171216cb3871588eae0c594bea
Your Name <you@example.com>
2022-02-14 08:42:36 +0300

    Init

---
+++ WeatherCache/OpenWeather/CurrentWeatherDto.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WeatherCache.OpenWeather
+{
+    public class Coord
+    {
+        [JsonPropertyName("lon")]
+        public double Longitude { get; set; }
+        [JsonPropertyName("lat")]
+        public double Latitude { get; set; }
+    }
+    public class Weather
+    {
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("icon")]
+        public string IconCode { get; set; }
+    }
+    public class Main
+    {
+        [JsonPropertyName("temp")]
+        public double TemperatureKelvin { get; set; }
+        [JsonPropertyName("feels_like")]
+        public double FeelsLikeKelvin { get; set; }
+    }
+    public class CurrentWeatherDto
+    {
+        [JsonPropertyName("coord")]
+        public Coord Coord { get; set; }
+
+        [JsonPropertyName("weather")]
+        public List<Weather> Weathers { get; set; }
+
+        [JsonPropertyName("main")]
+        public Main main { get; set; }
+
+        [JsonPropertyName("dt")]
+        public int LastUpdatedDateUnixSeconds { get; set; }
+    }
+}
---
+++ WeatherCache/OpenWeather/OpenWeatherClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace WeatherCache.OpenWeather
+{
+    public class OpenWeatherClient
+    {
+        const string defaultLanguage = "ru";
+        const string apiKey = "d0edb2714b11c310fcc847f5ec23e409";
+        const string urlTemplate = "https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}&lang={2}";
+
+        public async ValueTask<CurrentWeatherDto> GetWeatherAsync(string cityName)
+        {
+            var lowerCasedCityName = cityName.ToLower();
+
+            string currentWeatherUrl = string.Format(urlTemplate, lowerCasedCityName, apiKey, defaultLanguage);
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(currentWeatherUrl);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception($"Openweathermap response has a fault code {response.StatusCode}");
+            var currentWeatherJson = await response.Content.ReadAsStringAsync();
+
+            JsonDocument currentWeatherDocument = JsonDocument.Parse(currentWeatherJson);
+            return currentWeatherDocument.Deserialize<CurrentWeatherDto>();
+        }
+    }
+}
